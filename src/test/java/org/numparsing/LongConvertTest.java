d4e27ab542e9f6ddaf377509c012a2ff48b0493b
Daniel Gyoerffy <judalion777@gmail.com>
2022-06-27 19:07:19 +0200

    :white_check_mark: Add tests

--- src/test/java/org/numparsing/LongConvertTest.java
+++ src/test/java/org/numparsing/LongConvertTest.java
@@ -1,11 +1,15 @@
 package org.numparsing;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LongConvertTest {
-    @Test
-    
+    @ParameterizedTest
+    @CsvFileSource(resources = {"integerNumbers.csv"})
     void shouldParseStringAppropriately(String numberString, int radix) {
-
+        var underTest = new LongConvert();
+        assertEquals(Long.parseLong(numberString, radix), underTest.parseLong(numberString, radix));
     }
 }
